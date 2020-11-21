800d480547c0b0f8e699d6802f4720769d8fc547
EstebanOG <juanesteban199911@gmail.com>
2020-11-21 01:12:23 -0500

    Versión 1

---
+++ ArbolB/src/arbolb/Lista.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolb;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Lista {
+     // instance variables - replace the example below with your own
+    public static ArrayList<Integer> ingresados;
+
+
+    public Lista()
+    {
+       ingresados = new ArrayList<Integer>();
+    }
+    public boolean buscar(int valor){
+        boolean esta = false;
+        for (int i = 0; i < ingresados.size() && !esta; i++){
+            if (ingresados.get(i) == valor){
+                 esta = true;
+                
+            }
+        }
+        return esta;
+        
+    }
+}
---
+++ ArbolB/src/arbolb/Nodo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolb;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo {
+    public int[] valores;
+    public Nodo[] nodo;
+    public static int numValores;
+    public boolean tengoHijos = false;
+    public int ocupados = 0;
+    public Nodo padre;
+
+    public Nodo() {
+        nodo = new Nodo[Raiz.grado * 2 + 3];
+        valores = new int[Raiz.grado * 2 + 1];
+    }
+}
